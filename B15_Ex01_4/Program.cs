0e4085724faad2477e4c31e917d56a8f34b76099
yafim <ifim.vo@gmail.com>
2015-04-22 09:31:16 +0300

    palindrome class

--- B15_Ex01_4/Program.cs
+++ B15_Ex01_4/Program.cs
@@ -4,36 +4,97 @@ namespace B15_Ex01_4
 {
     public class Program
     {
+        static int sumOfNumbersInString;
+        static int sumOfCamelInString;
+
         public static void Main()
         {
             string inputFromUser;
 
-            inputFromUser
+            inputFromUser = getInputFromUser();
 
-
+            Console.WriteLine("{0}", isPalindrome(inputFromUser));
+            Console.WriteLine("Sum of Digits: {0}", sumOfNumbersInString);
+            Console.WriteLine("Sum of Chars: {0}", sumOfCamelInString);
+            Console.ReadLine();
         }
 
         //check if valid string was given
-        private static bool isValidString(string i_InputFromUser)
+        private static bool isValidString(string i_InputFromUser, ref int io_SumOfNumbersInString, ref int io_SumOfCamelInString)
         {
-            return i_InputFromUser.Length == 10;
+
+            if (i_InputFromUser.Length != 10)
+            {
+                return false;
+            }
+
+            bool isValidString = true;
+            bool containsNumbers = Char.IsNumber(i_InputFromUser[1]);
+            
+            for (int i = 0; i < 10 && isValidString; i++)
+            {
+                if (containsNumbers)
+                {
+                    if (Char.IsNumber(i_InputFromUser[i]))
+                    {
+                        isValidString = true;
+                        double numericValue = Char.GetNumericValue(i_InputFromUser[i]);
+                        io_SumOfNumbersInString += Convert.ToInt32(numericValue);
+                    }
+                }
+                else
+                {
+                    isValidString = isValidChar(i_InputFromUser[i]);
+
+                    if (Char.IsUpper(i_InputFromUser[i]))
+                    {
+                        io_SumOfCamelInString++;
+                    }
+                }
+            }
+            return isValidString;
         }
 
-        private static string getInputFromUser(string i_InputFromUser)
+        //check if valid english letter [a-Z]
+        private static bool isValidChar(char i_charToCheck)
         {
+            return (i_charToCheck >= 'A' && i_charToCheck <= 'Z') || (i_charToCheck >= 'a' && i_charToCheck <= 'z');
+        }
+
+        
+        private static string getInputFromUser()
+        {
+            string i_InputFromUser;
+
             while (true)
             {
                 Console.WriteLine("Please enter a string with 10 chars");
                 i_InputFromUser = Console.ReadLine();
 
-                if (!isValidString(i_InputFromUser))
+                if (isValidString(i_InputFromUser, ref sumOfNumbersInString, ref sumOfCamelInString))
                 {
-                    Console.WriteLine("This is not a valid string! let's try again");
+                    break;
                 }
-                break;
+                Console.WriteLine("Invalid Input! Try again...");
+                
             }
                 return i_InputFromUser;
             
         }
+
+        //reverse string
+        private static string reverseString(string i_String)
+        {
+            char[] reversedString = i_String.ToCharArray();
+            Array.Reverse(reversedString);
+            return new string(reversedString);
+        }
+        
+        private static bool isPalindrome(string i_String)
+        {
+                string reversedString = reverseString(i_String);
+                return reversedString.Equals(i_String);
+        }
+
     }
 }
