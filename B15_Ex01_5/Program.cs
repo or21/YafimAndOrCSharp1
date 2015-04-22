7b390a8186d24f79ddc34a95e28a6ee4de4f9147
yafim <ifim.vo@gmail.com>
2015-04-22 23:07:07 +0300

    styling...

--- B15_Ex01_5/Program.cs
+++ B15_Ex01_5/Program.cs
@@ -1,22 +1,38 @@
-﻿using System;
+﻿//-----------------------------------------------------------------------
+// <copyright file="Program.cs" company="B15_Ex01">
+// Yafim Vodkov 308973882 Or Brand id
+// </copyright>
+//-----------------------------------------------------------------------
+using System;
 using System.Linq;
 
 namespace B15_Ex01_5
 {
+    /// <summary>
+    /// this is header
+    /// </summary>
     public class Program
     {
-        static int s_NumbersGreaterThanFirst = 0;
-        static int s_NumbersSmallerThanFirst = 0;
+        /// <summary>
+        /// how many numbers are greater than the first number in the strings
+        /// </summary>
+        private static int s_NumbersGreaterThanFirst = 0;
+
+        /// <summary>
+        /// how many numbers are smaller than the first number in the strings
+        /// </summary>
+        private static int s_NumbersSmallerThanFirst = 0;
         
+        /// <summary>
+        /// runs the program.
+        /// </summary>
         public static void Main()
         {
-
             string inputFromUser = getInput();
             countGreaterAndSmallerNumbers(inputFromUser);
 
             char minValue = inputFromUser.Min();
             char maxValue = inputFromUser.Max();
-
 
             Console.WriteLine("Number Greater than 1st: {0} ", s_NumbersGreaterThanFirst);
             Console.WriteLine("Number Smaller than 1st: {0} ", s_NumbersSmallerThanFirst);
@@ -25,39 +41,61 @@ namespace B15_Ex01_5
             Console.ReadLine();
         }
 
-        //get input from the user
-        private static string getInput(){
+        /// <summary>
+        /// get input from the user
+        /// </summary>
+        /// <returns> valid 8 digit string </returns>
+        private static string getInput()
+        {
             string inputFromUser;
+
             while (true)
             {
                 Console.WriteLine("Please enter 8 digit string: ");
                 inputFromUser = Console.ReadLine();
+
                 if (isValidString(inputFromUser))
                 {
                     break;
                 }
+
                 Console.WriteLine("Invalid input! please try again...");
             }
+
             return inputFromUser;
         }
 
-        //check if valid string was given
+        /// <summary>
+        /// check if valid string was given
+        /// </summary>
+        /// <param name="i_StringToCheck">string to check</param>
+        /// <returns> true if valid string</returns>
         private static bool isValidString(string i_StringToCheck)
         {
             return i_StringToCheck.Length == 8 && containsOnlyNumbers(i_StringToCheck);
         }
-        //check if cotains only numbers
+
+        /// <summary>
+        /// check if string contains only numbers
+        /// </summary>
+        /// <param name="i_StringToCheck">string to check</param>
+        /// <returns> true if contains only numbers </returns>
         private static bool containsOnlyNumbers(string i_StringToCheck)
         {
             bool isNumber = false;
+
             for (int i = 0; i < i_StringToCheck.Length; i++)
             {
-                isNumber = Char.IsNumber(i_StringToCheck[i]);
+                isNumber = char.IsNumber(i_StringToCheck[i]);
             }
+
             return isNumber;
         }
 
-        //count how many numbers greater than first number
+        /// <summary>
+        /// counts how many numbers greater and smaller than first number
+        /// </summary>
+        /// <param name="i_StringOfNumbers">string to check</param>
         private static void countGreaterAndSmallerNumbers(string i_StringOfNumbers)
         {
             int numberToCompare;
@@ -75,16 +113,18 @@ namespace B15_Ex01_5
                 {
                       s_NumbersSmallerThanFirst++;
                 }
-                
             }
         }
 
-        //get numeric value from char
+        /// <summary>
+        /// get numerical value from char
+        /// </summary>
+        /// <param name="i_CharToNumericValue">char to convert</param>
+        /// <returns> char to integer </returns>
         private static int getNumValue(char i_CharToNumericValue)
         {
-            double numericValue = Char.GetNumericValue(i_CharToNumericValue);
+            double numericValue = char.GetNumericValue(i_CharToNumericValue);
             return Convert.ToInt32(numericValue);
         }
-        
     }
 }
